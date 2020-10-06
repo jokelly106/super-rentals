b5ba1186f87e70f87927885add493d5e845a40d0
jokelly106 <johnokelly106@gmail.com>
2020-10-06 11:00:49 -0400

    change application adapter to fetch from API
    App now fetches from localhost:3000 (default ruby server env for super_rentals_api). No Cors support yet, so must use modified browser to prevent cors issue.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,11 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import { pluralize } from 'ember-inflector';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  namespace = 'api';
+  host = "http://localhost:3000";
 
-  buildURL(...args) {
-    return `${super.buildURL(...args)}.json`;
+  pathForType = function (t) {
+    var underscored = Ember.String.underscore(t);
+    return pluralize(underscored);
   }
 }
