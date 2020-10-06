c543396fbfb9c78ab9e3c95a74c268c87c881398
jokelly106 <johnokelly106@gmail.com>
2020-10-06 18:03:18 -0400

    return empty object instead of store object?
    This fixes a data store caching issue, but is not great code because if any changes happen to the rental model it will create problems.

--- app/routes/rental-form-new.js
+++ app/routes/rental-form-new.js
@@ -5,6 +5,23 @@ export default class RentalFormNewRoute extends Route {
     @service store;
 
     async model() {
+        
+        return {
+            title: '',
+            owner: '',
+            city: '',
+            location: {
+                lat: 0,
+                lng: 0
+            },
+            category: '',
+            image: 'https://www.easylinedrawing.com/wp-content/uploads/2019/07/house_drawing.png',
+            bedrooms: 0,
+            description: ''
+        }
+        // Should create from store?
+        // Creates caching issue when cancelling creation...
+        // Unsure how to handle this case gracefully.
         let newRental = this.store.createRecord('rental');
         newRental.location = {
             lat: 0,
