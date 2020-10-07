ed3b6bf796695c55b66386a1a4a35c5a040549e2
jokelly106 <johnokelly106@gmail.com>
2020-10-07 15:02:52 -0400

    remove type field as it's not necessary.
    Type is a derived field from the category of the rental, there is not need to add it in the form.
    Also add a comment to index.js regarding our handling of dangling models.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 export default class IndexRoute extends Route {
     @service store;
     async model() {
+        //@TODO use hooks to unload any uncompleted rentals instead of unload all in future
         this.store.unloadAll();
         return this.store.findAll('rental');
     }
